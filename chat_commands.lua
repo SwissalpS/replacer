f9c85ea285c223429db0cb81e391a4d6c37a0914
Luke aka SwissalpS <Luke@SwissalpS.ws>
2021-12-02 22:27:47 +0100

    add chatcommand

---
+++ chat_commands.lua
@@ -0,0 +1,34 @@
+
+local rb = replacer.blabla
+
+replacer.chatcommand_mute = {
+
+	params = rb.ccm_params,
+	description = rb.ccm_description,
+	func = function(name, param)
+
+		local player = minetest.get_player_by_name(name)
+		if not player then
+			return false, rb.ccm_player_not_found
+		end
+		local meta = player:get_meta()
+		if not meta then
+			return false, rb.ccm_player_meta_error
+		end
+
+		local lower = string.lower(param)
+		if 'on' == lower then
+			meta:set_int('replacer_mute', 1)
+		elseif 'off' == lower then
+			meta:set_int('replacer_mute', 0)
+		else
+			return false, rb.ccm_hint
+		end
+
+		return true, ''
+
+	end
+}
+
+minetest.register_chatcommand('replacer_mute', replacer.chatcommand_mute)
+
