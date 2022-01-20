4e0f7e88abe3038e124a483b2c98db2370342176
Luke aka SwissalpS <Luke@SwissalpS.ws>
2022-01-20 06:00:42 +0100

    adding first use of alias map

--- compat/vines.lua
+++ compat/vines.lua
@@ -2,9 +2,12 @@ if not minetest.get_modpath('vines') then return end
 
 replacer.group_placeholder['group:vines'] = 'vines:vines'
 local vines = { 'jungle', 'root', 'side', 'vine', 'willow' }
-local node_name
+local name_base, name_end, name_middle
 for _, name in ipairs(vines) do
-    node_name = 'vines:' .. name .. '_end'
-	replacer.register_exception(node_name, node_name)
+    name_base = 'vines:' .. name
+    name_end = name_base .. '_end'
+    name_middle = name_base .. '_middle'
+	replacer.register_exception(name_end, name_end)
+	replacer.register_non_creative_alias(name_middle, name_end)
 end
 
