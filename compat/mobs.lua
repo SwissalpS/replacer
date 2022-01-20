b439d5cad5b843a926bd14f982a748c5422b5d4f
Luke aka SwissalpS <Luke@SwissalpS.ws>
2022-01-20 03:29:01 +0100

    oopsie

--- compat/mobs.lua
+++ compat/mobs.lua
@@ -1,7 +1,7 @@
--- for some reason cobwebs need this [mobs_animal]
+-- for some reason cobwebs need this [mobs_monster]
 -- probably because they drop string when dug and string
 -- is only a craft item.
-if minetest.get_modpath('mobs_animal') then
+if minetest.get_modpath('mobs_monster') then
 	replacer.register_exception('mobs:cobweb', 'mobs:cobweb')
 end
 
