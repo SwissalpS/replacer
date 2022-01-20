f5decde21bd705c515cfe001fe7ec867b3839a34
Luke aka SwissalpS <Luke@SwissalpS.ws>
2022-01-20 06:17:21 +0100

    whitespace

--- compat/vines.lua
+++ compat/vines.lua
@@ -4,9 +4,9 @@ replacer.group_placeholder['group:vines'] = 'vines:vines'
 local vines = { 'jungle', 'root', 'side', 'vine', 'willow' }
 local name_base, name_end, name_middle
 for _, name in ipairs(vines) do
-    name_base = 'vines:' .. name
-    name_end = name_base .. '_end'
-    name_middle = name_base .. '_middle'
+	name_base = 'vines:' .. name
+	name_end = name_base .. '_end'
+	name_middle = name_base .. '_middle'
 	replacer.register_exception(name_end, name_end)
 	replacer.register_non_creative_alias(name_middle, name_end)
 end
