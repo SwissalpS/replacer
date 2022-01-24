bf8975ec2600e50e02ef7e73aed15a1f95cab85a
Luke aka SwissalpS <Luke@SwissalpS.ws>
2022-01-24 15:06:13 +0100

    prefab compat no longer needed

--- compat/prefab.lua
+++
@@ -1,7 +0,0 @@
-if not minetest.get_modpath('prefab') then return end
-
-local name = 'prefab:concrete_with_grass'
-replacer.register_exception(name, name)
--- not adding the inverted slabs as they can't be rotated
--- anyway, and there are the table-saw slabs too.
-
