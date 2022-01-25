c387f5ade2f9cb3b5f6b19140bc618b4a46e0fcb
Luke aka SwissalpS <Luke@SwissalpS.ws>
2022-01-25 03:19:09 +0100

    compat for letters

---
+++ compat/letters.lua
@@ -0,0 +1,44 @@
+if not minetest.get_modpath('letters') then return end
+
+-- for inspection tool
+local function add_recipe_u(item_name, _, recipes)
+	if not item_name or not 'string' == type(item_name) then return end
+
+	local input, letter = item_name:match('^(.+)_letter_(.)u$')
+	if not input then return end
+
+	recipes[#recipes + 1] = {
+		method = 'cutting',
+		type = 'letters:upper',
+		items = { input },
+		output = item_name,
+		letter = letter
+	}
+end -- add_recipe_u
+
+replacer.register_craft_method('letters:upper', 'letters:letter_cutter_upper', add_recipe_u)
+
+
+local function add_recipe_l(item_name, _, recipes)
+	if not item_name or not 'string' == type(item_name) then return end
+
+	local input, letter = item_name:match('^(.+)_letter_(.)l$')
+	if not input then return end
+
+	recipes[#recipes + 1] = {
+		method = 'cutting',
+		type = 'letters:lower',
+		items = { input },
+		output = item_name,
+		letter = letter
+	}
+end -- add_recipe_l
+
+replacer.register_craft_method('letters:lower', 'letters:letter_cutter_lower', add_recipe_l)
+
+
+-- for replacer
+replacer.register_set_enabler(function(node)
+	return node and node.name and node.name:find('^.+_letter_(.)[lu]$')
+end)
+
