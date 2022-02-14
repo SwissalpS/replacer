3c4ae2aed154afd816762600c69bf0bc74de5022
Luke aka SwissalpS <Luke@SwissalpS.ws>
2022-02-14 16:05:48 +0100

    add support for improved scifi_nodes holders
    https://github.com/pandorabox-io/pandorabox_custom/blob/master/scifi_override.lua
    
    https://github.com/D00Med/scifi_nodes/pull/22

--- compat/scifi_nodes.lua
+++ compat/scifi_nodes.lua
@@ -10,34 +10,44 @@ replacer.register_exception('scifi_nodes:laptop_open', 'scifi_nodes:laptop_close
 
 -- These are not 100% accurate as other items could be dropped on the holders.
 -- We can't be more accurate as long as scifi_nodes does't store the item's name.
+-- Like https://github.com/pandorabox-io/pandorabox_custom/blob/master/scifi_override.lua
+-- does. When this override isn't installed, up to 9 objects are returned.
 
 local function add_recipe_itemholder(item_name, context, recipes)
     if 'scifi_nodes:itemholder' ~= item_name then return end
 
     if not (context and context.pos) then return end
 
-	local objects = minetest.get_objects_inside_radius(context.pos, .5)
-	if (not objects) or (0 == #objects) then return end
+	local held_name = minetest.get_meta(context.pos):get_string('item')
+	local items
+	if '' ~= held_name then
+		items = { held_name }
+	else
+		-- servers without override need to search for dropped items.
+		items = {}
+		local luaentity
+		local objects = minetest.get_objects_inside_radius(context.pos, .5)
+		if (not objects) or (0 == #objects) then return end
 
-	local held_name, luaentity
-	for _, obj in ipairs(objects) do
-		if obj and obj.get_luaentity then
-			luaentity = obj:get_luaentity()
-			if luaentity and luaentity.itemstring
-				and ('' ~= luaentity.itemstring)
-				and minetest.registered_items[luaentity.itemstring]
-			then
-				held_name = luaentity.itemstring
-				break
+		for _, obj in ipairs(objects) do
+			if obj and obj.get_luaentity then
+				luaentity = obj:get_luaentity()
+				if luaentity and luaentity.itemstring
+					and ('' ~= luaentity.itemstring)
+					and minetest.registered_items[ItemStack(luaentity.itemstring):get_name()]
+				then
+					table.insert(items, luaentity.itemstring)
+				end
 			end
+			if 9 == #items then break end
 		end
+		if 0 == #items then return end
 	end
-	if not held_name then return end
 
 	recipes[#recipes + 1] = {
 		method = S('holding'),
 		type = 'scifi_nodes:itemholder',
-		items = { held_name },
+		items = items,
 		output = nil,
 	}
 end -- add_recipe_itemholder
@@ -51,29 +61,37 @@ local function add_recipe_powered_stand(item_name, context, recipes)
 
     if not (context and context.pos) then return end
 
-	local objects = minetest.get_objects_inside_radius(
-		vector.add(context.pos, vector.new(0, 1, 0)), .5)
-	if (not objects) or (0 == #objects) then return end
+	local held_name = minetest.get_meta(context.pos):get_string('item')
+	local items
+	if '' ~= held_name then
+		items = { held_name }
+	else
+		-- servers without override need to search for dropped items.
+		items = {}
+		local luaentity
+		local objects = minetest.get_objects_inside_radius(
+			vector.add(context.pos, vector.new(0, 1, 0)), .5)
+		if (not objects) or (0 == #objects) then return end
 
-	local held_name, luaentity
-	for _, obj in ipairs(objects) do
-		if obj and obj.get_luaentity then
-			luaentity = obj:get_luaentity()
-			if luaentity and luaentity.itemstring
-				and ('' ~= luaentity.itemstring)
-				and minetest.registered_items[luaentity.itemstring]
-			then
-				held_name = luaentity.itemstring
-				break
+		for _, obj in ipairs(objects) do
+			if obj and obj.get_luaentity then
+				luaentity = obj:get_luaentity()
+				if luaentity and luaentity.itemstring
+					and ('' ~= luaentity.itemstring)
+					and minetest.registered_items[ItemStack(luaentity.itemstring):get_name()]
+				then
+					table.insert(items, luaentity.itemstring)
+				end
 			end
+			if 9 == #items then break end
 		end
+		if 0 == #items then return end
 	end
-	if not held_name then return end
 
 	recipes[#recipes + 1] = {
 		method = S('holding'),
 		type = 'scifi_nodes:powered_stand',
-		items = { held_name },
+		items = items,
 		output = nil,
 	}
 end -- add_recipe_powered_stand
