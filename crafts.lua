d50912760a2d7685b4a237eeb33cd0645e728846
Luke aka SwissalpS <Luke@SwissalpS.ws>
2024-07-21 04:15:13 +0200

    use local variable (since we already have it)

--- crafts.lua
+++ crafts.lua
@@ -1,9 +1,9 @@
 local r = replacer
 local rm = r.materials
 
-if not replacer.hide_recipe_basic then
+if not r.hide_recipe_basic then
 	minetest.register_craft({
-		output = replacer.tool_name_basic,
+		output = r.tool_name_basic,
 		recipe = {
 			{ rm.chest, '', rm.gold_ingot },
 			{ '', rm.mese_crystal_fragment, '' },
@@ -14,21 +14,21 @@ end
 
 
 -- only if technic mod is installed
-if replacer.has_technic_mod then
-	if not replacer.hide_recipe_technic_upgrade then
+if r.has_technic_mod then
+	if not r.hide_recipe_technic_upgrade then
 		minetest.register_craft({
-			output = replacer.tool_name_technic,
+			output = r.tool_name_technic,
 			recipe = {
-				{ replacer.tool_name_basic, 'technic:green_energy_crystal', '' },
+				{ r.tool_name_basic, 'technic:green_energy_crystal', '' },
 				{ '', '', '' },
 				{ '', '', '' },
 			}
 		})
 	end
-	if not replacer.hide_recipe_technic_direct then
+	if not r.hide_recipe_technic_direct then
 		-- direct upgrade craft
 		minetest.register_craft({
-			output = replacer.tool_name_technic,
+			output = r.tool_name_technic,
 			recipe = {
 				{ rm.chest, 'technic:green_energy_crystal', rm.gold_ingot },
 				{ '', rm.mese_crystal_fragment, '' },
